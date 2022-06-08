fe0e180dbd150959919a12ee1f59b6054142f893
gingerails <rocketcityabi@gmail.com>
2022-06-08 14:53:35 -0400

    created tests

---
+++ src/test/java/org/exerciseone/ItemTest.java
@@ -0,0 +1,48 @@
+package org.exerciseone;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ItemTest {
+
+    @Test
+    void getItemId() {
+    }
+
+    @Test
+    void setItemId() {
+    }
+
+    @Test
+    void getItemDescription() {
+    }
+
+    @Test
+    void setItemDescription() {
+    }
+
+    @Test
+    void getItemBrand() {
+    }
+
+    @Test
+    void setItemBrand() {
+    }
+
+    @Test
+    void getItemPrice() {
+    }
+
+    @Test
+    void setItemPrice() {
+    }
+
+    @Test
+    void returnAllInfo() {
+    }
+
+    @Test
+    void compare() {
+    }
+}
---
+++ src/test/java/org/exerciseone/MainTest.java
@@ -0,0 +1,14 @@
+package org.exerciseone;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @org.junit.jupiter.api.Test
+    void streamFilter() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void main() {
+    }
+}
