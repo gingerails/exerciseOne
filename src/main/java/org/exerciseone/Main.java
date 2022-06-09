4e429732900a8330fb7872f0c0241f733b83ed9b
gingerails <rocketcityabi@gmail.com>
2022-06-09 14:24:35 -0400

    JSON parsing commented out, back to hardcoded values

--- src/main/java/org/exerciseone/Main.java
+++ src/main/java/org/exerciseone/Main.java
@@ -62,17 +62,17 @@ public class Main {
 
         // VERY LAZY ADD ITEMS. Was going to make a JSON file which would be parsed but that was not the
         // point of this exercise and would have wasted today's time.
-//        inventoryList.add(new Item("FFA123", "Toyota", "Brake Pads for 2011 Prius C", 45.99));
-//        inventoryList.add(new Item("FFA124", "Toyota", "Brake Pads for 2011 Prius Standard", 45.99));
-//        inventoryList.add(new Item("CAI231", "OxiClean", "OxiClean Airvent CLeaner", 7.99));
-//        inventoryList.add(new Item("TWD612", "Traction", "Push Pull Cable Hardware Kits - H/D Truck", 3.39));
-//        inventoryList.add(new Item("TWD108", "Traction", "Wheel Chock - Rubber", 32.49));
-//        inventoryList.add(new Item("TWD051", "Traction", "Brake Shoes - Single / Bulk", 31.99));
-//        inventoryList.add(new Item("TWD626", "Traction", "Push Pull Cable Hardware Kits - H/D Truck", 3.39));
-//        inventoryList.add(new Item("BK7051", "NAPA", "Hose Clamps Standard T Bolt", 7.99));
-//        inventoryList.add(new Item("GAT271", "Gates", "Hydraulic Hose Hydraulic Hose Very High Pressure", 19.99));
-//        inventoryList.add(new Item("BK8128", "Mi-T-M", "Mi-T-M Pressure Washer, Gas Powered, Hot Water, 3500 PSI", 6689.00));
-        readFile();
+        inventoryList.add(new Item("FFA123", "Toyota", "Brake Pads for 2011 Prius C", 45.99));
+        inventoryList.add(new Item("FFA124", "Toyota", "Brake Pads for 2011 Prius Standard", 45.99));
+        inventoryList.add(new Item("CAI231", "OxiClean", "OxiClean Airvent CLeaner", 7.99));
+        inventoryList.add(new Item("TWD612", "Traction", "Push Pull Cable Hardware Kits - H/D Truck", 3.39));
+        inventoryList.add(new Item("TWD108", "Traction", "Wheel Chock - Rubber", 32.49));
+        inventoryList.add(new Item("TWD051", "Traction", "Brake Shoes - Single / Bulk", 31.99));
+        inventoryList.add(new Item("TWD626", "Traction", "Push Pull Cable Hardware Kits - H/D Truck", 3.39));
+        inventoryList.add(new Item("BK7051", "NAPA", "Hose Clamps Standard T Bolt", 7.99));
+        inventoryList.add(new Item("GAT271", "Gates", "Hydraulic Hose Hydraulic Hose Very High Pressure", 19.99));
+        inventoryList.add(new Item("BK8128", "Mi-T-M", "Mi-T-M Pressure Washer, Gas Powered, Hot Water, 3500 PSI", 6689.00));
+        //readFile();
         streamFilter();
 
 
